cdc2da0f3bf9409e8d530cca094e793a34abb827
paopaosoftwire <paopao.chuenchoksan@softwire.com>
2023-06-27 12:04:37 +0100

    fix optional recursion compute fib number function

--- src/fibonacci.ts
+++ src/fibonacci.ts
@@ -1,35 +1,34 @@
 export const computeFibonacciNumber = (position: number, recursion: boolean = false): number => {
     if (recursion) {
         return recursiveFibonacci(1, 1, position - 2);
-
-        let notNullPosition = position;
-        if (notNullPosition === null) {
-            notNullPosition = 1;
-        }
-
-        let i = 1;
-        let j = 1;
-
-        if (notNullPosition === 0) {
-            return 0;
-        }
-        if (notNullPosition < 0) {
-            return computeNegativeFibonacci(notNullPosition);
-        }
-
-        if (notNullPosition <= 2) {
-            return 1;
-        }
-
-        let currentPosition = 2;
-        while (currentPosition < notNullPosition) {
-            const temp = i;
-            i = j;
-            j += temp;
-            currentPosition++;
-        }
-        return j;
     }
+    let notNullPosition = position;
+    if (notNullPosition === null) {
+        notNullPosition = 1;
+    }
+
+    let i = 1;
+    let j = 1;
+
+    if (notNullPosition === 0) {
+        return 0;
+    }
+    if (notNullPosition < 0) {
+        return computeNegativeFibonacci(notNullPosition);
+    }
+
+    if (notNullPosition <= 2) {
+        return 1;
+    }
+
+    let currentPosition = 2;
+    while (currentPosition < notNullPosition) {
+        const temp = i;
+        i = j;
+        j += temp;
+        currentPosition++;
+    }
+    return j;
 }
 
 const recursiveFibonacci = (previous: number, current: number, stepsLeft: number): number => {
